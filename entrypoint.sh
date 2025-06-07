e13cd4b472f8607f3ddde2f666a0aea6c5e0993a
TaquitoSuabe <eduardotaquin@gmail.com>
2025-06-07 05:24:39 -0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/sbin/sshd -D &
 echo "Iniciando proxy en el puerto $PORT, redirigiendo a 127.0.0.1:22"
-/usr/local/bin/app -p "$PORT" -l 22 -s 0
+/usr/local/bin/app -p "$PORT" -l 22 -s 2
