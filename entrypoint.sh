67011a8f1a80c7082cc8225099d2cdedd8391c85
TaquitoSuabe <eduardotaquin@gmail.com>
2025-06-07 06:01:59 -0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Iniciando proxy en segundo plano..."
-/usr/local/bin/app -p "$PORT" -l 22 -s 1 &
+/usr/local/bin/app -p "$PORT" -l 22 -s 2 &
 echo "Iniciando servidor SSH Dropbear en puerto 22..."
 /usr/sbin/dropbear -R -E -F -p 22
